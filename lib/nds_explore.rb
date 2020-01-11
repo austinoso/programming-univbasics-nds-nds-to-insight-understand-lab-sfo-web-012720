8faba86c8ab0ca9e13fd4ac38f8ca1932e8b5417
Austin Osorio <osorio_austin@yahoo.com>
2020-01-11 04:05:38 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,7 +9,12 @@ end
 
 def print_first_directors_movie_titles
 
-  directors_database[0][movies][0][title]
+  movie_index = 1
+  
+  while movie_index < directors_database[0][:movies].length do
+    puts directors_database[0][:movies][movie_index][:title]
+    movie_index += 1
+  end
 end
 
-puts directors_database[0][:movies][0][:title]
+puts 
